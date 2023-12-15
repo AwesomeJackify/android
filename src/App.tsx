e469cfc281a7aecc784e444bb9328dc77eb7cea3
Jack Gong <jackgong48@gmail.com>
2023-12-15 11:20:02 +0700

    FIXED: double clicking of instagram icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
 import instagramImg from "./assets/instagram.png";
 import portfolioFeed from "./assets/portfolioFeed.png";
 
@@ -7,49 +8,52 @@ function App() {
   const timeline1 = gsap.timeline({ paused: true });
   const timeline2 = gsap.timeline({ paused: true });
 
-  timeline1.to(".box1", {
-    width: "100%",
-    height: "100%",
-    ease: "expo.inOut",
-    duration: 0.5,
-  });
-  timeline1.to(
-    "#instagram",
-    {
-      visibility: "hidden",
-      duration: 0,
-    },
-    "<"
-  );
-  timeline1.to(
-    "#portfolioFeed",
-    {
-      visibility: "visible",
-      duration: 0,
-    },
-    "<"
-  );
+  useGSAP(() => {
+    timeline1.to(".box1", {
+      width: "100%",
+      height: "100%",
+      ease: "expo.inOut",
+      duration: 0.5,
+    });
+    timeline1.to(
+      "#instagram",
+      {
+        display: "none",
+        duration: 0,
+      },
+      "<"
+    );
+    timeline1.to(
+      "#portfolioFeed",
+      {
+        display: "block",
+        duration: 0,
+      },
+      "<"
+    );
 
-  timeline2.to(".box1", {
-    width: "13rem",
-    height: "13rem",
-    ease: "expo.inOut",
-    duration: 0.5,
+    timeline2.to(".box1", {
+      width: "13rem",
+      height: "13rem",
+      ease: "expo.inOut",
+      duration: 0.5,
+    });
+    timeline2.to(
+      "#portfolioFeed",
+      {
+        display: "none",
+        duration: 0.3,
+      },
+      "<"
+    );
+    timeline2.to(
+      "#instagram",
+      {
+        display: "block",
+      },
+      "<0.3"
+    );
   });
-  timeline2.to(
-    "#portfolioFeed",
-    {
-      visibility: "hidden",
-    },
-    "<.3"
-  );
-  timeline2.to(
-    "#instagram",
-    {
-      visibility: "visible",
-    },
-    "<"
-  );
 
   return (
     <div className="h-screen flex w-screen relative items-end justify-center">
@@ -57,21 +61,19 @@ function App() {
         className="w-10 h-10 bg-red-500 absolute top-0 right-0 z-10"
         onClick={() => timeline2.restart()}
       ></div>
-      <div
-        className="w-52 h-52 cursor-pointer box1 relative"
-        onClick={() => timeline1.restart()}
-      >
+      <div className="w-52 h-52 box1 relative">
         <img
           src={instagramImg}
           id="instagram"
           alt="instagram icon"
-          className="absolute top-0 left-0 w-full h-full object-cover"
+          className="absolute w-full h-full cursor-pointer object-cover"
+          onClick={() => timeline1.restart()}
         />
         <img
           src={portfolioFeed}
-          alt="portfolio feed"
           id="portfolioFeed"
-          className="absolute top-0 left-0 w-full h-full object-cover invisible"
+          alt="portfolio feed"
+          className="absolute w-full h-full cursor-pointer object-cover -z-10 hidden"
         />
       </div>
     </div>
