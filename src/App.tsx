e439e791c9970583a72d30ba8690d321ee160384
Jack Gong <jackgong48@gmail.com>
2023-12-15 08:42:51 +0700

    ADDED: instagram opening

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,78 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import { gsap } from "gsap";
-gsap.to(".box", { x: 200 });
+import instagramImg from "./assets/instagram.png";
+import portfolioFeed from "./assets/portfolioFeed.png";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const timeline1 = gsap.timeline({ paused: true });
+  const timeline2 = gsap.timeline({ paused: true });
 
-  let tween = gsap
-    .to(".box1", {
-      duration: 0.5,
-      width: "100%",
-      height: "100%",
-      ease: "expo.inOut",
-    })
-    .pause();
+  timeline1.to(".box1", {
+    width: "100%",
+    height: "100%",
+    ease: "expo.inOut",
+    duration: 0.5,
+  });
+  timeline1.to(
+    "#instagram",
+    {
+      visibility: "hidden",
+      duration: 0,
+    },
+    "<"
+  );
+  timeline1.to(
+    "#portfolioFeed",
+    {
+      visibility: "visible",
+      duration: 0,
+    },
+    "<"
+  );
+
+  timeline2.to(".box1", {
+    width: "13rem",
+    height: "13rem",
+    ease: "expo.inOut",
+    duration: 0.5,
+  });
+  timeline2.to(
+    "#portfolioFeed",
+    {
+      visibility: "hidden",
+    },
+    "<.3"
+  );
+  timeline2.to(
+    "#instagram",
+    {
+      visibility: "visible",
+    },
+    "<"
+  );
 
   return (
-    <div className="h-screen flex w-screen relative">
+    <div className="h-screen flex w-screen relative items-end justify-center">
       <div
         className="w-10 h-10 bg-red-500 absolute top-0 right-0 z-10"
-        onClick={() => tween.reverse()}
+        onClick={() => timeline2.restart()}
       ></div>
-      <div className="flex w-full justify-evenly items-end">
-        {/* <div className="w-52 h-52 bg-green-500 cursor-pointer"></div> */}
-        <div
-          className="w-52 h-52 bg-green-500 cursor-pointer box1"
-          onClick={() => tween.play()}
-        ></div>
+      <div
+        className="w-52 h-52 cursor-pointer box1 relative"
+        onClick={() => timeline1.restart()}
+      >
+        <img
+          src={instagramImg}
+          id="instagram"
+          alt="instagram icon"
+          className="absolute top-0 left-0 w-full h-full object-cover"
+        />
+        <img
+          src={portfolioFeed}
+          alt="portfolio feed"
+          id="portfolioFeed"
+          className="absolute top-0 left-0 w-full h-full object-cover invisible"
+        />
       </div>
     </div>
   );
