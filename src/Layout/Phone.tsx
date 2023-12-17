b476522e9fa9d79cba007d05edce029e52bdc40e
songexile <therunningbeta@gmail.com>
2023-12-18 12:25:58 +1300

    Added bar

--- src/Layout/Phone.tsx
+++ src/Layout/Phone.tsx
@@ -6,9 +6,14 @@ const Phone = ({ children }) => {
     <div className="fixed h-screen  flex w-screen items-center justify-center bg-black">
       <div
         id="phone"
-        className="min-h-screen  overflow-auto no-scrollbar relative  my-16 w-full h-full md:w-1/3  flex items-center justify-center bg-white rounded-md shadow-md"
+        className="min-h-screen flex-col items-center  overflow-auto no-scrollbar relative   w-full h-full md:w-1/3  flex  justify-start bg-white rounded-md shadow-md"
       >
-        <main className="mb-32">{children}</main>
+        <div className="bg-red-600 flex justify-evenly items-center w-full h-8">
+          <h1>iPod</h1>
+          <h1>15:31</h1>
+          <h1>25%</h1>
+        </div>
+        <main className="mt-16">{children}</main>
       </div>
     </div>
   );
